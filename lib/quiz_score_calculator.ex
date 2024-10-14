6850ecf7b463a7c1802f773f5cd52efc14efc33e
mars <163072113+cadet-dot-net@users.noreply.github.com>
2024-10-14 18:19:03 +0100

    init mix project

---
+++ lib/quiz_score_calculator.ex
@@ -0,0 +1,18 @@
+defmodule QuizScoreCalculator do
+  @moduledoc """
+  Documentation for `QuizScoreCalculator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> QuizScoreCalculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/quiz_score_calculator_test.exs
@@ -0,0 +1,8 @@
+defmodule QuizScoreCalculatorTest do
+  use ExUnit.Case
+  doctest QuizScoreCalculator
+
+  test "greets the world" do
+    assert QuizScoreCalculator.hello() == :world
+  end
+end
