3812e8c0d817a5a33aae490db26c7c2b95d5c6a7
mars <163072113+cadet-dot-net@users.noreply.github.com>
2024-10-14 19:01:20 +0100

    capture IO output for tests - add example test json files

--- test/quiz_score_calculator_test.exs
+++ test/quiz_score_calculator_test.exs
@@ -1,3 +1,41 @@
 defmodule QuizScoreCalculatorTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  @doc """
+  {
+    "enjoys_job": "yes",
+    "good_for_carers": "yes",
+    "contracted_hours": 20,
+    "hours_actually_worked": 34,
+    "unpaid_extra_work": "unsure",
+    "age": 26,
+    "hourly_rate": "£8.22",
+    "submitted_date": 1608211454000
+  }
+  """
+  test "missing values in file are not assigned points" do
+    assert capture_io(fn ->
+             QuizScoreCalculator.score("./test/quiz_responses/response_missing.json")
+           end) =~ "Score is 2/5 (40%)"
+  end
+
+  @doc"""
+  {
+    "enjoys_job": "yes",
+    "repected_by_managers": "yes",
+    "good_for_carers": "yes",
+    "contracted_hours": 20,
+    "hours_actually_worked": 27,
+    "unpaid_extra_work": "no",
+    "age": 26,
+    "hourly_rate": "£8.91",
+    "submitted_date": 1608211454000
+  }
+  """
+  test "all values are assigned points correctly" do
+    assert capture_io(fn ->
+             QuizScoreCalculator.score("./test/quiz_responses/response_100.json")
+           end) =~ "Score is 5/5 (100%)"
+  end
 end
